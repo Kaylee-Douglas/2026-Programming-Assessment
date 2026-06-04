0179023f280cb729a6e334b266e1c0877debbb37
Kaylee-Douglas <22081@nayland.school.nz>
2026-06-04 14:32:32 +1200

    Create main window

--- 2026 Programming Assessment.py
+++ 2026 Programming Assessment.py
@@ -1,1 +1,13 @@
-import tkinter as tk #import tkinter
+import tkinter as tk
+
+Main_Image = "testimg.png"
+
+def create_widget():
+    root = tk.Tk() #create window
+    root.title("Wellbeing Widget") #name window
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    create_widget()
+
