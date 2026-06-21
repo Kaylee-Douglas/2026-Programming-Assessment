ff36c06b1f32ed1eb3e06831d7005383c0f7a3c0
Kaylee-Douglas <22081@nayland.school.nz>
2026-06-21 14:42:17 +1200

    run back git history to remove accidentally added file (which broke everything!) and re add deleted code - including moving the widget into a class, connecting to database and re commenting code.

--- 2026 Programming Assessment.py
+++ 2026 Programming Assessment.py
@@ -1,42 +1,56 @@
-import tkinter as tk
+import tkinter as tk # import gui so graphical interface can be used
+import mysql.connector as mysql # connect database to vscode
+# note: database is LOCAL and will not work on other computers without seperate setup
+import datetime # allow for time to be tracked
 
-Main_Image = "testimg.png"
+# Creates the widget as a class (allows for more features)
+class WellbeingWidget:
+    def __init__(self, image_path="testimg.png"):
+        self.image_path = image_path
+        self.root = tk.Tk()
+        
+        # adds class function that..
+        self.setup_window() # creates the main window
+        self.load_image() # loads the main image so that it can be used
+        self.place_widget() # places the widget on the screen
+        self.bind_events() # causes the widget to close when clicked
 
-def create_widget():
-    root = tk.Tk() #create window
-    root.title("Wellbeing Widget") #name window
-    root.attributes('-transparentcolor', 'white')  # Make background color transparent
-    root.overrideredirect(True) #remove window title
-    root.attributes("-topmost", False) #ensure windows can open over
+    def setup_window(self):
+        self.root.title("Wellbeing Widget") # name window
+        self.root.attributes('-transparentcolor', 'white') #removes plain white color, allowing backgrounds to be transparent
+        self.root.overrideredirect(True) # removes framing
+        self.root.attributes("-topmost", False) # allows other tabs to open over, keeping it on the desktop.
 
-    # Load image
-    img = tk.PhotoImage(file=Main_Image) #load image in window
+    def load_image(self):
+        self.img = tk.PhotoImage(file=self.image_path) # loads image as whatever file was specified 
+        self.label = tk.Label(self.root, image=self.img) # names the image for use
+        self.label.image = self.img # ensures the image is not labeled unused and not displayed (thanks tkinter)
+        self.label.pack() # make the image actually display using all the information it has been given
 
-    label = tk.Label(root, image=img)
-    label.image = img
-    label.pack()
+    def place_widget(self):
+        screen_width = self.root.winfo_screenwidth() # find width of the screen
+        screen_height = self.root.winfo_screenheight() #find height of the screen
 
-    # Screen and image size
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+        img_width = self.img.width() # find width of the image
+        img_height = self.img.height() # find height of the image
 
-    img_width = img.width() #define width of img
-    img_height = img.height() #define height of img
+        x = screen_width - img_width - 10 # store image placement as 10 pixels from the right side of the screen
+        y = screen_height - img_height - 40 # store image placement as 40 pixels from the bottom of the screen (greater b/c of the taskbar)
 
-    # place on screen
-    x = screen_width - img_width - 10
-    y = screen_height - img_height - 40  # w/ taskbar considered
+        self.root.geometry(f"{img_width}x{img_height}+{x}+{y}") #set the image to the right size and place it
 
-    root.geometry(f"{img_width}x{img_height}+{x}+{y}") #sets window using x, y and image size values
+    # lets the widget close when clicked rather than forcing the user to end the code manually
+    def bind_events(self):
+        self.label.bind("<Button-1>", lambda e: self.close())
 
-    # widget closes on left click (temp.)
-    label.bind("<Button-1>", lambda e: root.destroy())
+    def close(self):
+        self.root.destroy()
 
-    root.mainloop() 
+    def run(self):
+        self.root.mainloop()
 
-print("Kachow")
-create_widget()
 
-# create image on screen
-# change image using clock function
-# afford state to image so that when in 'bad' state opens 
+# Run the widget
+if __name__ == "__main__": #if the program is run
+    widget = WellbeingWidget("testimg.png") # create a widget using the test image (changeable for skins/states later!)
+    widget.run() # go go go !!
